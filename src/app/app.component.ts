cab25205de4687f88b2153949c8ed21998321100
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-01-07 18:35:00 -0300

    feat: add new task:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 import { Todo } from 'src/models/todos.models';
 
 @Component({
@@ -19,24 +20,38 @@ export class AppComponent {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       title: ['', Validators.compose([
-        Validators.minLength(11),
-        Validators.maxLength(11),
-        Validators.required
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
       ])]
     });
 
-    this.todos.push(new Todo(1, 'Arrumar o quarto', false))
-    this.todos.push(new Todo(1, 'Estudar angular', false))
-    this.todos.push(new Todo(1, 'Ir para faculdade', true))
+    // this.todos.push(new Todo(1, 'Arrumar o quarto', false))
+    // this.todos.push(new Todo(2, 'Estudar angular', false))
+    // this.todos.push(new Todo(3, 'Ir para faculdade', true))
+  }
+
+  add() {
+    const title = this.form.controls['title'].value; // valor inserido no campo de pesquisa
+    const id = this.todos.length+1;
+    this.todos.push(new Todo(id, title, false))
+
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
     if(index !== -1) this.todos.splice(index, 1);
   }
+
   MarkAsDone(todo:Todo) {
     todo.done = true;
   }
+
   MarkAsUndone(todo: Todo) {
     todo.done = false;
   }
