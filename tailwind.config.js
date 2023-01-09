392dee4397985f87b9e7cd6e16d3bb4c149cd6ae
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-01-09 17:12:10 -0300

    about: style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,vue},',
+    './src/**/*.{html,css,vue}',
     './public/index.html'
   ],
   theme: {
