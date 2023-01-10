3f940367a130c3bfb1f337ed592dd147ac85be95
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-01-10 19:14:24 -0300

    about: axios and components added

---
+++ src/components/TodoSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+   <div class="text-center">
+      <img
+         src="@/assets/img/spinnerr.svg"
+         alt=""
+         class="inline-block w-5 h-5"
+      />
+   </div>
+</template>
