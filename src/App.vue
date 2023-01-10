a5f73eb8a8815a74420fdee56c31eccbe169154a
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-01-10 19:33:51 -0300

    about: axios config

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
    <!-- Content -->
    <div class="px-3 py-10 md:px-10">
       <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
          <!-- Todo spinner -->
          <TodoSpinner />
 
@@ -20,12 +21,12 @@
 </template>
 
 <script>
-// import TodoItems from '@/components/TodoItems.vue'
-import TodoForm from '@/components/TodoForm.vue'
-import TodoSpinner from '@/components/TodoSpinner.vue'
-// import TodoItem from '@/components/TodoItem.vue'
-import TodoEmpty from '@/components/TodoEmpty.vue'
-
+// import TodoItems from '@/components/TodoItems.vue';
+import TodoForm from '@/components/TodoForm.vue';
+import TodoSpinner from '@/components/TodoSpinner.vue';
+// import TodoItem from '@/components/TodoItem.vue';
+import TodoEmpty from '@/components/TodoEmpty.vue';
+import axios from 'axios';
 
 export default {
    name: "App",
@@ -37,10 +38,16 @@ export default {
       // TodoItem,
       TodoEmpty
    },
-   
-   created: {
-      
-   }
+
+   data() {
+      return {
+         todos: []
+      }
+   },
+
+   async created() {
+      this.todos = await axios.get('http://localhost:3000/todos')
+      .then((response) => response.data)
+   },
 };
 </script>
-
