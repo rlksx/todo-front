cdee993e92567acaed926b60546f2fefc26fb2f8
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-01-12 17:36:14 -0300

    feat: update state

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,12 @@ export default createStore({
       },
 
       storeTodo(state, payload) {
-         state.todos.unshift(payload);
+         const index = state.todos.findIndex((todo) => todo.id == payload.id);
+         if (index >= 0) {
+            state.todos.splice(index, 1, payload);
+         } else {
+            state.todos.unshift(payload);
+         }
       },
    },
 
@@ -40,10 +45,14 @@ export default createStore({
             });
       },
 
-      updateTodo(context, { id, data }) {
-         return axios.put(`http://localhost:3000/todos/${id}`, data);
-      }, 
+      updateTodo({ commit }, { id, data }) {
+         return axios
+            .put(`http://localhost:3000/todos/${id}`, data)
+            .then((response) => {
+               commit("storeTodo", response.data);
+            });
+      },
    },
-
+ 
    modules: {},
 });
